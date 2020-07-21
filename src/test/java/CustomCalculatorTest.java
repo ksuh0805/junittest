08d4ca9756751a77fcbcf796134cee28d5ee5aa7
suhyun <ksuh0805@ajou.ac.kr>
2020-07-22 01:37:38 +0900

    junit test 과제 테스트 코드 추가

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -2,6 +2,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -14,8 +16,12 @@ public class CustomCalculatorTest {
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertTrue(result == 25);
+        assertThat(result, is(25));
         System.out.println("result :: " + result);
+
+        assertFalse(result == 2);
     }
 
     //빼기 테스트 작성
@@ -23,8 +29,21 @@ public class CustomCalculatorTest {
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertTrue(result == 13);
+        assertThat(result, is(13));
         System.out.println("result :: " + result);
+
+        assertFalse(result != 13);
+    }
+
+    @Test
+    public void 빼기테스트(){
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(23,10);
+        assertFalse(result == 12);
+
+        assertThat(result, is(13));
     }
 
     //곱하기 테스트 작성
@@ -32,8 +51,10 @@ public class CustomCalculatorTest {
     public void multiply() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
         System.out.println("result :: " + result);
+
+        assertThat(result, is(45));
     }
 
     //나누기 테스트 작성
@@ -41,7 +62,35 @@ public class CustomCalculatorTest {
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
         System.out.println("result :: " + result);
+
+        assertTrue(result == 5);
+    }
+
+    @Test(timeout = 4000)
+    public void timeInMethodTest() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
+    @Test(timeout = 3000)
+    public void 시간제한체크() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIsEmptyIndexOutOfBoundException(){
+        new ArrayList<Object>().get(0);
+
+        ArrayList<Object> newList = new ArrayList<>();
+        Object object = newList.get(0);
+    }
+
+    @Test
+    public void isEmptyArray(){
+        ArrayList<Object> myList = new ArrayList<>();
+        assertThat(myList, is(empty()));
+
+        assertTrue(myList.isEmpty());
     }
 }
--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -15,7 +15,10 @@ public class UpperCaseCounterTest {
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
+
+        assertTrue(numberOfUpperCaseCharactersInString == 0);
     }
 
     //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
@@ -23,7 +26,10 @@ public class UpperCaseCounterTest {
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString, is(0));
+
+        assertTrue(numberOfUpperCaseCharactersInString == 0);
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -34,8 +40,13 @@ public class UpperCaseCounterTest {
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
 
         //assertTrue로 맞는 테스트 코드 작성
+        assertTrue(numberOfUpperCaseCharactersInString == 10);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(numberOfUpperCaseCharactersInString == 0);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(numberOfUpperCaseCharactersInString, is(10));
+
+        assertTrue(numberOfUpperCaseCharactersInString > 0);
     }
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
@@ -44,20 +55,26 @@ public class UpperCaseCounterTest {
         String str = "ABCdefGHI";
         int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
         //assertTrue로 맞는 테스트 코드 작성
+        assertTrue(result == 6);
         //assertFalse로 틀리는 값을 넣어 테스트 작성
+        assertFalse(result == 3);
         //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(result, is(6));
         System.out.println("result :: " + result);
+
+        assertFalse(result > 7);
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+    @Test(timeout = 6000)
+    @Ignore
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
